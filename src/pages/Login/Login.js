6fe127d14ac9315100ac47c1e5014597d38f4d25
marcela-celani <marcelacelani.ti@gmail.com>
2023-07-19 21:17:08 -0300

    pratica guiada de custom hooks para formularios

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,22 +2,27 @@ import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { irParaCadastro } from '../../Routes/coordinator'
 import { ContainerForm, ContainerLogin, Input } from './styled'
+import useForm from '../../hooks/useForm'
 
 export default function Login() {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+  // const [email, setEmail] = useState("")
+  // const [password, setPassword] = useState("")
+  
+
+  // const onChangeEmail = (e) => {
+  //   setEmail(e.target.value)
+  // }
+  // const onChangeSenha = (e) => {
+  //   setPassword(e.target.value)
+  // }
   const navigate = useNavigate()
 
-  const onChangeEmail = (e) => {
-    setEmail(e.target.value)
-  }
-  const onChangeSenha = (e) => {
-    setPassword(e.target.value)
-  }
+  const {form, onChangeInputs, clearFields} = useForm({email:'', password:''})
 
   const enviaLogin = (e) => {
     e.preventDefault()
-    console.log({ email, password })
+    console.log(form)
+    clearFields()
   }
 
   return (
@@ -25,17 +30,25 @@ export default function Login() {
       <ContainerForm onSubmit={enviaLogin}>
         <label htmlFor='email'>Email:</label>
         <Input
+          name='email'
           id='email'
-          value={email}
-          onChange={onChangeEmail}
+          type='email'
+          value={form.email}
+          onChange={onChangeInputs}
           placeholder="nome@email.com"
+          required
         />
         <label htmlFor='senha'>Senha:</label>
         <Input
+          name='password'
           id='senha'
-          value={password}
-          onChange={onChangeSenha}
+          type='password'
+          value={form.password}
+          onChange={onChangeInputs}
           placeholder="Digite sua senha"
+          pattern='^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$'
+          title='A senha deve possuir letras maiúsculas, minúsculas, números e caracteres especiais.'
+          required
         />
         <button>Fazer Login</button>
       </ContainerForm>
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,62 +1,84 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { ContainerForm, ContainerSignup, Input } from './styled'
+import useForm from '../../hooks/useForm'
 
 export default function Signup() {
-    const [nomeUsuario, setNomeUsuario] = useState("")
-    const [email, setEmail] = useState("")
-    const [senha, setSenha] = useState("")
-    const [confirmaSenha, setConfirmaSenha] = useState("")
+    // const [nomeUsuario, setNomeUsuario] = useState("")
+    // const [email, setEmail] = useState("")
+    // const [senha, setSenha] = useState("")
+    // const [confirmaSenha, setConfirmaSenha] = useState("")
 
-    const onChangeNome = (e) => {
-        setNomeUsuario(e.target.value)
-    }
-    const onChangeEmail = (e) => {
-        setEmail(e.target.value)
-    }
-    const onChangeSenha = (e) => {
-        setSenha(e.target.value)
-    }
-    const onChangeConfirmaSenha = (e) => {
-        setConfirmaSenha(e.target.value)
-    }
+    // const onChangeNome = (e) => {
+    //     setNomeUsuario(e.target.value)
+    // }
+    // const onChangeEmail = (e) => {
+    //     setEmail(e.target.value)
+    // }
+    // const onChangeSenha = (e) => {
+    //     setSenha(e.target.value)
+    // }
+    // const onChangeConfirmaSenha = (e) => {
+    //     setConfirmaSenha(e.target.value)
+    // }
 
-    const enviarCadastro = () => {
+    const enviarCadastro = (e) => {
         //* EXTRA: validando a senha - ter certeza que o usuário sabe qual senha cadastrou
-        if (senha === confirmaSenha) {
-            console.log({nomeUsuario, email, senha, confirmaSenha})
+        e.preventDefault()
+        if (form.senha === form.confirmaSenha) {
+            console.log(form)
+            clearFields()
+        } else {
+            alert('O campo de senha e confirmação de senha devem ser iguais!')
         }
+        console.log(form)
+        alert('Usuário cadastrado com sucesso!')
     }
+
+    const {form, onChangeInputs, clearFields} = useForm({nomeUsuario:'', email:'', senha:'', confirmaSenha:''})
+
 
     return (
         <ContainerSignup>
             <ContainerForm onSubmit={enviarCadastro}>
                 <label htmlFor='nome'>Nome de usuario:</label>
                 <Input
+                    name='nomeUsuario'
                     id='nome'
-                    value={nomeUsuario}
-                    onChange={onChangeNome}
+                    value={form.nomeUsuario}
+                    onChange={onChangeInputs}
                     placeholder="username"
+                    type='text'
+                    required
                 />
                 <label htmlFor='email'>Email:</label>
                 <Input
+                    name='email'
                     id='email'
-                    value={email}
-                    onChange={onChangeEmail}
+                    value={form.email}
+                    onChange={onChangeInputs}
                     placeholder="nome@email.com"
+                    type='email'
+                    required
                 />
                 <label htmlFor='senha'>Senha:</label>
                 <Input
+                    name='senha'
                     id='senha'
-                    value={senha}
-                    onChange={onChangeSenha}
+                    value={form.senha}
+                    onChange={onChangeInputs}
                     placeholder="Crie sua senha"
+                    type='password'
+                    required
                 />
                 <label htmlFor='confirma-senha'>Confirmação de senha:</label>
                 <Input
+                    name='confirmaSenha'
                     id='confirma-senha'
-                    value={confirmaSenha}
-                    onChange={onChangeConfirmaSenha}
+                    value={form.confirmaSenha}
+                    onChange={onChangeInputs}
                     placeholder="Confirme a senha"
+                    type='password'
+                    required
                 />
                 <button>Cadastrar</button>
             </ContainerForm>
